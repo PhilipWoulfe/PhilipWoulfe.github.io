382e760ca758fe52e737d1697b95f2b2cc5aef41
PhilipWoulfe <philip.woulfe@gmail.com>
2018-06-18 18:15:44 +0100

    Messed Up Brackets

--- script.js
+++ script.js
@@ -188,15 +188,16 @@ function getWorldCupScores() {
       if (dir == "asc") {
         if (isNum) {
 			if (Number(x.innerHTML) > Number(y.innerHTML)) {
-			  // If so, mark as a switch and break the loop:
-			  shouldSwitch = true;
-			  break;
+				// If so, mark as a switch and break the loop:
+				shouldSwitch = true;
+				break;
 			}
+		}
 		else {
 			if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-			  // If so, mark as a switch and break the loop:
-			  shouldSwitch = true;
-			  break;
+				// If so, mark as a switch and break the loop:
+				shouldSwitch = true;
+				break;
 			}
 		}
       } else if (dir == "desc") {
@@ -206,7 +207,7 @@ function getWorldCupScores() {
 			  shouldSwitch = true;
 			  break;
 			}
-		else {
+		} else {
 			if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
 			  // If so, mark as a switch and break the loop:
 			  shouldSwitch = true;
@@ -216,21 +217,19 @@ function getWorldCupScores() {
       }
     }
     if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-      and mark that a switch has been done: */
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-      // Each time a switch is done, increase this count by 1:
-      switchcount ++; 
+		/* If a switch has been marked, make the switch
+		and mark that a switch has been done: */
+		rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+		switching = true;
+		// Each time a switch is done, increase this count by 1:
+		switchcount ++; 
     } else {
-      /* If no switching has been done AND the direction is "asc",
-      set the direction to "desc" and run the while loop again. */
+		/* If no switching has been done AND the direction is "asc",
+		set the direction to "desc" and run the while loop again. */
 		if (switchcount == 0 && dir == "asc") {
 			dir = "desc";
 			switching = true;
 		}
-	  }
+		}
 	}
-  }
-  }
- }
+}
